07b68616b86bbdcee8eae671231475d5f1af743b
ja-chowder <j.sowers325@gmail.com>
2022-10-05 18:23:11 -0500

    ch5cont

---
+++ beaufort.c
@@ -0,0 +1,16 @@
+/* takes wind speed a provides description according to the Beaufort scale */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float windspeed;
+
+	printf("Enter a wind speed (mph): ");
+	scanf("%f", &windspeed;
+
+	if (windspeed < 1)
+		printf("Calm");
+	else if (windspeed >= 1 && windspeed <= 3)
+	}
+}
