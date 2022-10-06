fa9523e63f1584b6f9f9228bae5261bcf459ce8f
ja-chowder <j.sowers325@gmail.com>
2022-10-05 19:18:59 -0500

    ch5mas

--- beaufort.c
+++ beaufort.c
@@ -7,10 +7,20 @@ int main(void)
 	float windspeed;
 
 	printf("Enter a wind speed (mph): ");
-	scanf("%f", &windspeed;
+	scanf("%f", &windspeed);
 
 	if (windspeed < 1)
-		printf("Calm");
-	else if (windspeed >= 1 && windspeed <= 3)
-	}
+		printf("Calm\n");
+	else if (windspeed >= 1 && windspeed < 4)
+		printf("Light air\n");
+	else if (windspeed >= 4 && windspeed < 28)
+		printf("Breeze\n");
+	else if (windspeed >= 28 && windspeed < 48)
+		printf("Gale\n");
+	else if (windspeed >= 48 && windspeed < 63)
+		printf("Storm\n");
+	else
+		printf("Hurricane\n");
+
+	return 0;
 }
